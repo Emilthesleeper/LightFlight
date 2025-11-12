104092a8971bd8616fe01620b9e12a457b549762
Emilthesleeper <emilwege@icloud.com>
2025-11-12 22:06:49 +0100

    Initial commit 1.0

---
+++ src/client/java/com/emilsleeper/client/LightFlightClient.java
@@ -0,0 +1,48 @@
+package com.emilsleeper.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.network.packet.c2s.play.UpdatePlayerAbilitiesC2SPacket;
+import org.lwjgl.glfw.GLFW;
+
+
+public class LightFlightClient implements ClientModInitializer {
+    private static KeyBinding flightKey;
+    private void updateAbilitiesWithDelay(ClientPlayerEntity player) {
+        new Thread(() -> {
+            try {
+                Thread.sleep(41);
+                player.sendAbilitiesUpdate();
+                player.networkHandler.sendPacket(new UpdatePlayerAbilitiesC2SPacket(player.getAbilities()));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    @Override
+    public void onInitializeClient() {
+        flightKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "lightflight.keybind.flightKey",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_CAPS_LOCK,
+                "lightflight.category"
+        ));
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (flightKey.wasPressed()) {
+                if (client.player != null) {
+                    if (client.player.getAbilities().flying) {
+                        client.player.getAbilities().flying = false;
+                    } else {
+                        client.player.getAbilities().flying = true;
+                    }
+                    updateAbilitiesWithDelay(client.player);
+                }
+            }
+        });
+    }
+}
---
+++ src/main/java/com/emilsleeper/LightFlight.java
@@ -0,0 +1,10 @@
+package com.emilsleeper;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LightFlight implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
